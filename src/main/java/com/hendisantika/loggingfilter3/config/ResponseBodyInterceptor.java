a64fe663b1ecf8fcc4a77ae5dcbf49095055181d
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 08:13:48 +0700

    Add beforeBodyWrite in ResponseBodyInterceptor 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/loggingfilter3/config/ResponseBodyInterceptor.java
+++ src/main/java/com/hendisantika/loggingfilter3/config/ResponseBodyInterceptor.java
@@ -3,7 +3,12 @@ package com.hendisantika.loggingfilter3.config;
 import com.hendisantika.loggingfilter3.service.LoggingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -26,5 +31,13 @@ public class ResponseBodyInterceptor implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType,
+                                  MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        loggingService.displayResponse(((ServletServerHttpRequest) request).getServletRequest(), ((ServletServerHttpResponse) response).getServletResponse(), body);
+        return body;
     }
 }
