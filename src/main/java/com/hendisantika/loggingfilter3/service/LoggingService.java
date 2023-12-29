0ff343a8bbcbd78aecac4e46a27b3c65e771be2d
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 08:08:06 +0700

    Add displayReq in LoggingService 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/loggingfilter3/service/LoggingService.java
+++ src/main/java/com/hendisantika/loggingfilter3/service/LoggingService.java
@@ -1,7 +1,13 @@
 package com.hendisantika.loggingfilter3.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static sun.tools.jconsole.inspector.Utils.getParameters;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +22,22 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class LoggingService {
+    public void displayReq(HttpServletRequest request, Object body) {
+        StringBuilder reqMessage = new StringBuilder();
+        Map<String, String> parameters = getParameters(request);
 
+        reqMessage.append("REQUEST ");
+        reqMessage.append("method = [").append(request.getMethod()).append("]");
+        reqMessage.append(" path = [").append(request.getRequestURI()).append("] ");
+
+        if (!parameters.isEmpty()) {
+            reqMessage.append(" parameters = [").append(parameters).append("] ");
+        }
+
+        if (!Objects.isNull(body)) {
+            reqMessage.append(" body = [").append(body).append("]");
+        }
+
+        log.info("log Request: {}", reqMessage);
+    }
 }
