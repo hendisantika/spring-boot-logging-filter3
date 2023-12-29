5102f0af2a76f3fe6c6f2073be477afea96d8b5e
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 08:07:44 +0700

    Add LoggingService 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/loggingfilter3/service/LoggingService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.loggingfilter3.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/29/23
+ * Time: 08:07
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+public class LoggingService {
+
+}
