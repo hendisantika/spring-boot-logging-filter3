d048babe2faab31c493428d20c60e0820ec67226
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 08:00:34 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/loggingfilter3/SpringBootLoggingFilter3Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.loggingfilter3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingFilter3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingFilter3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/loggingfilter3/SpringBootLoggingFilter3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.loggingfilter3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingFilter3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
