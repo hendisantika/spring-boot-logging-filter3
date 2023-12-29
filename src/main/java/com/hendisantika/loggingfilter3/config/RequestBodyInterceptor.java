7e55d1a85660a0b41652046792d4e171cc6637dd
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 08:12:18 +0700

    Add afterBodyRead in RequestBodyInterceptor 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/loggingfilter3/config/RequestBodyInterceptor.java
+++ src/main/java/com/hendisantika/loggingfilter3/config/RequestBodyInterceptor.java
@@ -3,8 +3,13 @@ package com.hendisantika.loggingfilter3.config;
 import com.hendisantika.loggingfilter3.service.LoggingService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +28,10 @@ public class RequestBodyInterceptor extends RequestBodyAdviceAdapter {
     private final LoggingService logService;
 
     private final HttpServletRequest request;
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        logService.displayRequest(request, body);
+        return super.afterBodyRead(body, inputMessage, parameter, targetType, converterType);
+    }
 }
